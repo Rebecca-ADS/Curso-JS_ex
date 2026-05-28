ffe4e2dedeb912f963875f9d27bdaa68d30e9ace
Rebecca-ADS <rebeccaimoveisararaquara@gmail.com>
2026-05-27 23:08:37 -0300

    Novo exercício

--- _ex16.js
+++ _ex16.js
@@ -1,11 +1,6 @@
 /*16. Dada uma matriz M[1..6,1..8], criar um vetor C que contenha, em cada posição, a
 quantidade de elementos negativos da linha correspondente de M.*/
 
-
-/*16. Dada uma matriz M[1..6,1..8], criar um vetor C que contenha,
-em cada posição, a quantidade de elementos negativos da linha
-correspondente de M.*/
-
 let M = [
     [ 1, -2,  3, -4,  5,  6, -7,  8],
     [-1, -2,  3,  4, -5,  6,  7, -8],
