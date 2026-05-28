0bb089de825105619dda2a29d93cb1629a0e8cc7
Rebecca-ADS <rebeccaimoveisararaquara@gmail.com>
2026-05-27 23:07:41 -0300

    Novo exercício

--- _ex16.js
+++ _ex16.js
@@ -2,4 +2,36 @@
 quantidade de elementos negativos da linha correspondente de M.*/
 
 
+/*16. Dada uma matriz M[1..6,1..8], criar um vetor C que contenha,
+em cada posição, a quantidade de elementos negativos da linha
+correspondente de M.*/
+
+let M = [
+    [ 1, -2,  3, -4,  5,  6, -7,  8],
+    [-1, -2,  3,  4, -5,  6,  7, -8],
+    [ 1,  2,  3,  4,  5, -6, -7, -8],
+    [-1,  2, -3,  4, -5,  6,  7,  8],
+    [ 1,  2, -3, -4,  5,  6,  7, -8],
+    [-1, -2, -3, -4, -5,  6,  7,  8]
+];
+
+let C = [];
+
+for (let i = 0; i < 6; i++) {
+
+    let negativos = 0;
+
+    for (let j = 0; j < 8; j++) {
+
+        if (M[i][j] < 0) {
+            negativos++;
+        }
+    }
+
+    C[i] = negativos;
+}
+
+console.log("Vetor C:");
+console.log(C);
+
 
