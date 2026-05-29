9a9b31cca85194ec8ddf50b109a37937d72caace
Rebecca-ADS <rebeccaimoveisararaquara@gmail.com>
2026-05-29 17:37:55 -0300

    Atualiza exercício 18

--- _ex18.js
+++ _ex18.js
@@ -1,4 +1,17 @@
 //18. Crie um objeto chamado dados que contém várias propriedades, incluindo números,
 //strings e arrays. Escreva uma função que retorne um novo objeto apenas com as
 //propriedades que são arrays.
+const dados = {
+    nome: "Rebecca",
+    idade: 43,
+    cursos: ["ADS", "AWS", "JavaScript"],
+    cidade: "Araraquara",
+    notas: [8, 9, 10],
+    salario: 3500
+};
 
+const resultado = Object.fromEntries(
+    Object.entries(dados).filter(([chave, valor]) => Array.isArray(valor))
+);
+
+console.log(resultado);
