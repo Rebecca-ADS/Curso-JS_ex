ebf75a232fe99f66463fbafd28db34f8e201d3a1
Rebecca-ADS <rebeccaimoveisararaquara@gmail.com>
2026-05-29 17:08:21 -0300

    Adiciona exercício 18

--- _ex18.js
+++ _ex18.js
@@ -1,3 +1,4 @@
 //18. Crie um objeto chamado dados que contém várias propriedades, incluindo números,
 //strings e arrays. Escreva uma função que retorne um novo objeto apenas com as
 //propriedades que são arrays.
+
