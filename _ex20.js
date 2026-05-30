98ab5ff6890eb8c419e7f7cb51d062b0ec363d4e
Rebecca-ADS <rebeccaimoveisararaquara@gmail.com>
2026-05-29 23:59:33 -0300

    Adiciona exercicio 20

--- _ex20.js
+++ _ex20.js
@@ -1,7 +1,7 @@
 /*20. Dado um array de strings, crie um objeto onde
  cada string é uma chave, e seu valor é o
 número de vezes que a string aparece no array.*/
-0
+
 
 const frutas = [
     "maça",
